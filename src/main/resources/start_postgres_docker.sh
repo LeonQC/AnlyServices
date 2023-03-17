3f98cae3212a8853d3692bf76e3da65e4a5123d7
qy237 <qy237@cornell.edu>
2023-03-17 00:11:03 -0400

    Add Flyway to handle the schema migration for PostgreSQL

--- src/main/resources/start_postgres_docker.sh
+++ src/main/resources/start_postgres_docker.sh
@@ -1,5 +1,2 @@
 docker rm -f pgsql-dev
-docker run --name pgsql-dev -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=AnlyPostgres -d -p 5434:5432 postgres
-sleep 3
-docker cp ./schema.sql pgsql-dev:/docker-entrypoint-initdb.d/schema.sql
-docker exec -it pgsql-dev psql -U postgres -d AnlyPostgres -f docker-entrypoint-initdb.d/schema.sql
+docker run --name pgsql-dev -e POSTGRES_PASSWORD=123456 -e POSTGRES_DB=AnlyPostgres -d -p 5434:5432 postgres:14
